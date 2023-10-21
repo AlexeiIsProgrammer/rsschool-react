b847c80ce82e80d7160da76ee756e4279fd26685
Alexei <avenge.now.r@mail.ru>
2023-10-21 19:17:58 +0300

    feat: add husky to the project

--- src/app/index.test.tsx
+++ src/app/index.test.tsx
@@ -1,6 +1,6 @@
 import { describe, expect, it } from 'vitest';
 import { screen, render } from '@testing-library/react';
-import App from './App';
+import App from '.';
 
 describe('App', () => {
   it('should render hello world', () => {
