e5ededd25f4c60787b4d97a18966db0707e1492c
Alexei <avenge.now.r@mail.ru>
2023-10-29 12:57:13 +0300

    feat: add format:fix script

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,13 +2,13 @@
 
 /** @type {import("prettier").Config} */
 const config = {
-    "endOfLine": "auto",
-    "semi": true,
-    "singleQuote": true,
-    "tabWidth": 2,
-    "trailingComma": "es5",
-    "printWidth": 100,
-    "arrowParens": "always"
+  endOfLine: 'auto',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  printWidth: 100,
+  arrowParens: 'always',
 };
 
 module.exports = config;
