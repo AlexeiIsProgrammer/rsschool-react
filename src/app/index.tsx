fbd8ff98e87d2843c9f54e42760077e173b80cc8
Alexei <avenge.now.r@mail.ru>
2023-11-03 17:36:00 +0300

    feat: add routing to forms

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,28 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from '../pages/ErrorPage';
+import ReactHookFormPage from '../pages/ReactHookFormPage';
+import UncontrolledPage from '../pages/UncontrolledPage';
+import MainPage from '../pages/MainPage';
+import Layout from '../pages/Layout';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout component={<MainPage />} />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/react-hook-form',
+    element: <Layout component={<ReactHookFormPage />} />,
+  },
+  {
+    path: '/uncontrolled',
+    element: <Layout component={<UncontrolledPage />} />,
+  },
+]);
+
 function App() {
-  return <div>Hello App</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/UncontrolledPage/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function UncontrolledPage() {
+  return <div>UncontrolledPage</div>;
+}
