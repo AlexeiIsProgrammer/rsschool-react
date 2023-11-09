1bc91a2178401bd3902b47f23a0269172a923dac
Alexei <avenge.now.r@mail.ru>
2023-11-09 11:21:47 +0300

    fix: change structure of Input range

--- src/components/Searching/index.tsx
+++ src/components/Searching/index.tsx
@@ -5,12 +5,12 @@ import { PokemonURL } from '../../API/types/interfaces';
 import { ContainerWrapper } from '../../styles';
 import Alert from '../Alert';
 import FallbackUIButton from '../FallbackUIButton';
+import InputRange from '../InputRange';
+import Pagination from '../Pagination';
 import PokemonsList from '../PokemonsList';
+import SearchInput from '../SearchInput';
 import Spinner from '../Spinner';
 import { SearchingContainer, SearchingSizeContainer } from './styles';
-import Pagination from '../Pagination';
-import SearchInput from '../SearchInput';
-import InputRange from '../InputRange';
 
 function Searching() {
   const [loading, setLoading] = useState<boolean>(false);
