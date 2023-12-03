53376a0310970b0310328ecb35a023e634d15ae4
Alexei <avenge.now.r@mail.ru>
2023-12-03 14:37:33 +0300

    feat: modify password strength

--- src/components/UI/Input/index.tsx
+++ src/components/UI/Input/index.tsx
@@ -11,26 +11,28 @@ const Input = forwardRef<HTMLInputElement, Props>(({ error, name, ...props }, re
     1: 'one',
     2: 'two',
     3: 'three',
+    4: 'four',
   };
 
   return (
     <div className={styles.input}>
       <input {...props} ref={ref} />
-      {error &&
-        (name === 'password' ? (
-          <>
-            <div
-              className={`${styles.input__error_secure} ${styles[passwordStates[error.length]]}`}
-            >
-              {[1, 2, 3, 4].map((err) => (
-                <div className={styles.input__error_secure_item} key={err.toString()} />
-              ))}
-            </div>
-            <span className={styles.input__error}>{error[0]}</span>
-          </>
-        ) : (
-          <span className={styles.input__error}>{error}</span>
-        ))}
+      {name === 'password' ? (
+        <>
+          <div
+            className={`${styles.input__error_secure} ${
+              styles[passwordStates[error?.length || 4]]
+            }`}
+          >
+            {[1, 2, 3, 4].map((err) => (
+              <div className={styles.input__error_secure_item} key={err.toString()} />
+            ))}
+          </div>
+          <span className={styles.input__error}>{error && error[0]}</span>
+        </>
+      ) : (
+        <span className={styles.input__error}>{error}</span>
+      )}
     </div>
   );
 });
